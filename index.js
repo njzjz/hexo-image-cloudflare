4a732d55690c53c67b371bda136c9371b7594b05
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-03 15:30:21 -0500

    fix: max_width can be a list

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ const full_url_for = require('hexo-util').full_url_for.bind(hexo);
 const util = require('util');
 const url = require("url");
 const querystring = require('querystring');
+const tag = require('html-tag');
 var cdn_server = hexo.config.cdn_server || (hexo.config.cdn && hexo.config.cdn.server) || "https://images.weserv.nl";
 var cdn_prefix = cdn_server + "/?";
 var use_webp = hexo.config.cdn_use_webp || (hexo.config.cdn && hexo.config.cdn.use_webp) || false;
 var max_width = hexo.config.cdn && hexo.config.cdn.max_width;
+
+// convert number to list
+if(typeof max_width == "number"){
+  max_width = [max_width];
+} else if(max_width){
+  max_width.sort(function(a, b){return b - a});
+} else {
+  max_width = [];
+}
+max_width.push(null);
 
 function cdn_link(link, output=null, max_width=null){
   var obj = {
@@ -35,12 +46,16 @@ function replacer(match, p1, p2, offset, string) {
 	return util.format('![%s](%s)', p1, cdn_link(p2));
 }
 
-function source_tag(link, type=null){
+function source_tag(link, type=null, max_width=null){
+  var new_link = cdn_link(link, output=type, max_width = max_width);
+  var obj = {srcset: new_link};
   if(type){
-    return `<source srcset="${link}" type="${type}">`;
-  }else{
-    return `<source srcset="${link}">`;
+    obj.type = "image/" + type;
   }
+  if(max_width){
+    obj.media = `(max-width: ${max_width}px)`;
+  }
+  return tag('source', obj);
 }
 
 hexo.extend.injector.register('head_begin', `<link rel="preconnect" href="${cdn_server}">`);
@@ -61,11 +76,13 @@ if (use_webp || max_width){
 			}
       var link = parse_url(p2);
       var source_str = "";
-      if(use_webp){
-        source_str += source_tag(cdn_link(link, output='webp', max_width = max_width), type='image/webp');
-        source_str += source_tag(cdn_link(link, output='png', max_width = max_width), type='image/png');
-      } else {
-        source_str += source_tag(cdn_link(link, max_width = max_width));
+      for(var ii=0; ii<max_width.length; ii++){
+        if(use_webp){
+          source_str += source_tag(link, type='webp', max_width = max_width[ii]);
+          source_str += source_tag(link, type='png', max_width = max_width[ii]);
+        } else {
+          source_str += source_tag(link, max_width = max_width[ii]);
+        }
       }
 			return `<picture>${source_str}
 				${str.replace('<img', `<img webp-comp data-zoom-src="${p2}"`)}
